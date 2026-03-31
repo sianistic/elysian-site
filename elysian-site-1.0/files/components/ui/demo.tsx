0c27473852cf5b5a1c03139ef37f26f8dbf12831
SianEly <82179173+sianistic@users.noreply.github.com>
2026-03-31 00:54:22 -0400

    Delete elysian-site-1.0/files directory

--- elysian-site-1.0/files/components/ui/demo.tsx
+++
@@ -1,9 +0,0 @@
-import ShaderShowcase from "@/components/ui/hero"
-
-export default function DemoOne() {
-  return (
-    <div className="min-h-screen h-full w-full">
-      <ShaderShowcase />
-    </div>
-  )
-}
